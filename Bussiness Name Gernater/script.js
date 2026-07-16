5585de2dff57bc3c0c1a28b1d3b1f05ea77d48a3
Programming-with-Abhinav <workabhinav6@gmail.com>
2026-07-16 22:02:24 +0530

    Business name generator

---
+++ Bussiness Name Gernater/script.js
@@ -0,0 +1,25 @@
+let userID = "Abhinavsingh";
+let userlength = userID.length;
+console.log(`@${userID}${userlength}`);
+
+//Name calculation
+
+let adj = {
+    adj1: "Cazy",
+    adj2: "Amazing",
+    adj3: "Fire",
+};
+
+let shopNmae = {
+    shopNmae1: "Engine",
+    shopNmae2: "Foods",
+    shopNmae3: "Garments",
+}
+
+let anotherWord = {
+    anotherWrod1: "Bros",
+    anotherWrod2: "limited",
+    anotherWrod3: "Hub",
+}
+
+
