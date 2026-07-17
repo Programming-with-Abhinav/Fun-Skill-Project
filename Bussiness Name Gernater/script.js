e477b8bfb2d7eef348c5d8f5bf8fba34d36e2c4e
Programming-with-Abhinav <workabhinav6@gmail.com>
2026-07-17 12:21:02 +0530

    fix all problem and error. Code is ready

--- Bussiness Name Gernater/script.js
+++ Bussiness Name Gernater/script.js
@@ -1,25 +1,59 @@
-let userID = "Abhinavsingh";
-let userlength = userID.length;
-console.log(`@${userID}${userlength}`);
+/* Guess then Bussiness Name */
 
-//Name calculation
+/* Adjectives */
 
 let adj = {
-    adj1: "Cazy",
+    adj1: "Crazy",
     adj2: "Amazing",
     adj3: "Fire",
 };
 
-let shopNmae = {
-    shopNmae1: "Engine",
-    shopNmae2: "Foods",
-    shopNmae3: "Garments",
+
+let adjItem = (adj) => {
+    let values = Object.values(adj);
+    let randomIndex = Math.floor(Math.random() * values.length);
+    return values[randomIndex];        
+    
+}; 
+let randomAdj = adjItem(adj);
+ 
+/* Shop Name */
+
+let shopName = {
+    shopName1: "Engine",
+    shopName2: "Foods",
+    shopName3: "Garments",
 }
+
+let shopNameItem = (shopName) => {
+    let values = Object.values(shopName);
+    let randomIndex = Math.floor(Math.random() * values.length);
+    return values[randomIndex];
+};
+let randomShopName = shopNameItem(shopName);
+
+/* Another Wrod */
 
 let anotherWord = {
     anotherWrod1: "Bros",
     anotherWrod2: "limited",
     anotherWrod3: "Hub",
+} 
+
+let anotherWordItem = (anotherWord) => {
+    let values = Object.values(anotherWord);
+    let randomIndex = Math.floor(Math.random() * values.length);
+    return values[randomIndex];
 }
+let randomAnotherName = anotherWordItem(anotherWord);
 
+/* final Business Name */
 
+const BusinessName = `Business Name: ${randomAdj} ${randomShopName} ${randomAnotherName}`;
+console.log(BusinessName);
+
+/* Business ID */
+
+let BusinessID = prompt("Enter Your for Making good Business ID");
+let make_BuinessID =` @${BusinessID}${BusinessID.length} `;
+console.log(`Sir this Your Bussiness ID: ${make_BuinessID}`);
